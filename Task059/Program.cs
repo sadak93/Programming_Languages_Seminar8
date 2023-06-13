3cb4124d30ea412255b4bcc5decc6a3f1f1dfc94
Sadak <s.sadak02@gmail.com>
2023-06-13 21:03:49 +0600

    Удаление строки и столбца с наименьшим элементом

---
+++ Task059/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+// которая удалит строку и столбец, на пересечении которых расположен 
+// наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+// 9 2 3
+// 4 2 4
+// 2 6 7
+
+
+void DeleteDiagonalArray(int[,] Array)
+{
+    int min = Array[0,0];
+    int[] Position = new int[2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            if(Array[i,j] < min)
+            {
+                min = Array[i,j];
+                Position[0] = i;
+                Position[1] = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            if(i != Position[0] && j != Position[1])
+                System.Console.Write($"{Array[i,j],3} ");
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+    
+}
+
+void RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Array[i,j] = new Random().Next(1,21);
+    }
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            System.Console.Write($"{Array[i,j],3} ");
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(System.Console.ReadLine());
+int[,] Array = new int[rows,columns];
+
+RandomFillArray(Array);
+PrintArray(Array);
+DeleteDiagonalArray(Array);
