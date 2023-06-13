6a93c21d8832fd98c577afb9e11f16546c961a85
Sadak <s.sadak02@gmail.com>
2023-06-13 21:02:10 +0600

    Замена строки на столбцы

---
+++ Task055/Program.cs
@@ -0,0 +1,116 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, если это невозможно, 
+// программа должна вывести сообщение для пользователя.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(-30,31);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j],3}    "); 
+        System.Console.WriteLine();
+        }
+}
+
+void Transponirovanie(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = i; j < array.GetLength(1); j++)
+    {
+        int temp = array[i,j]; // здесь можно было сделать множетвенное присваивание
+        array[i,j] = array[j, i];
+        array [j,i] = temp;
+
+    }     
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+if (array.GetLength(0) == array.GetLength(1))
+{
+    Transponirovanie(array);
+    PrintArray(array);
+}
+else 
+{
+    System.Console.WriteLine("Матрица не квадратная");
+}
+
+// int EnterNumber(string message)
+// {
+//     int number = -1;
+//     while (number == -1)
+//     {
+//         System.Console.Write(message);
+//         try
+//         {
+//             number = Convert.ToInt32(Console.ReadLine());
+//             if (number < 1)
+//             {
+//                 System.Console.WriteLine("Введённое вами число не является натуральным.");
+//                 number = -1;
+//             }
+//         }
+//         catch
+//         {
+//             Console.WriteLine("То, что вы ввели, не является целым числом!");
+//         }
+//     }
+//     return number;
+// }
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = i; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(-30, 31);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i, j],3}    ");
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void ReplaceRowsAndCols(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = i; j < array.GetLength(1); j++)
+//         {
+//             int buf = array[i, j];
+//             array[i, j] = array[j, i];
+//             array[j, i] = buf;
+//         }
+// }
+
+// Console.Clear();
+// int rows = EnterNumber("Введите количество строк: ");
+// int cols = EnterNumber("Введите количество столбцов: ");
+// if (rows != cols) System.Console.WriteLine("Заменить строки на столбцы невозможно.");
+// else
+// {
+//     int[,] array = new int[rows, cols];
+//     FillArray(array);
+//     PrintArray(array);
+//     ReplaceRowsAndCols(array);
+//     System.Console.WriteLine();
+//     PrintArray(array);
+// }
